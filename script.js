4f0bd24eb17866601aa5211a9ac6f4ebf7889326
Jenna Qian <jiayin1996@gmail.com>
2021-10-09 17:20:53 -0400

    minor changes

--- script.js
+++ script.js
@@ -3,36 +3,38 @@ const displayContent = document.querySelector("#display-content");
 let createForecastToday = document.createElement("div");
 let createForecastTomorrow = document.createElement("div");
 let createForecastNextTwoDays = document.createElement("div");
-const ul = document.querySelector("ul")
+const ul = document.querySelector("ul");
 
 const forecast = document.querySelector("#forecast");
 forecast.append(
-  createForecastToday,
-  createForecastTomorrow,
-  createForecastNextTwoDays
-);
-
-getWeatherBtn.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const input = document.querySelector("#location-input").value;
-  getWeather(input);
-});
-
-function getWeather(inputCity) {
-  let url = `https://wttr.in/${inputCity}?format=j1`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-    //   console.log(data);
-
-      let cityHeading = data.nearest_area[0].areaName[0].value;
-      let cityRegion = data.nearest_area[0].region[0].value;
-      let cityCountry = data.nearest_area[0].country[0].value;
-      let cityCurrentlyFeelsLifeInFarenheit =
-        data.current_condition[0].FeelsLikeF;
-
-      displayContent.innerHTML = `
+    createForecastToday,
+    createForecastTomorrow,
+    createForecastNextTwoDays
+    );
+    
+    getWeatherBtn.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        const input = document.querySelector("#location-input").value;
+        getWeather(input);
+    });
+    
+    let cityCurrentlyFeelsLifeInFarenheit;
+    let cityHeading;
+    
+    function getWeather(inputCity) {
+        let url = `https://wttr.in/${inputCity}?format=j1`;
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            //   console.log(data);
+            
+            cityHeading = data.nearest_area[0].areaName[0].value;
+            let cityRegion = data.nearest_area[0].region[0].value;
+            let cityCountry = data.nearest_area[0].country[0].value;
+            cityCurrentlyFeelsLifeInFarenheit = data.current_condition[0].FeelsLikeF;
+            
+            displayContent.innerHTML = `
             <h2>${cityHeading}</h2>
             <p class="display-data">
             <span><strong>Area: </strong>${cityHeading}</span>
@@ -40,49 +42,55 @@ function getWeather(inputCity) {
             <span><strong>Country: </strong>${cityCountry}</span>
             <span><strong>Currently: </strong>Feels Like ${cityCurrentlyFeelsLifeInFarenheit}&#176;F</span>
             </p> 
-        `;
-
-      let todayAvgTemp = data.weather[0].avgtempF;
-      let todayMaxTemp = data.weather[0].maxtempF;
-      let todayMinTemp = data.weather[0].mintempF;
-      let tomorrowAvgTemp = data.weather[1].avgtempF;
-      let tomorrowMaxTemp = data.weather[1].maxtempF;
-      let tomorrowMinTemp = data.weather[1].mintempF;
-      let nextTwoDaysAvgTemp = data.weather[2].avgtempF;
-      let nextTwoDaysMaxTemp = data.weather[2].maxtempF;
-      let nextTwoDaysMinTemp = data.weather[1].mintempF;
-
-      createForecastToday.innerHTML = `
-        <h4>Today</h4>
-        <p class="display-today">
-        <span><strong>Average Temperature: </strong>${todayAvgTemp}&#176;F</span>
-        <span><strong>Max Temperature: </strong>${todayMaxTemp}&#176;F</span>
-        <span><strong>Min Temperature: </strong>${todayMinTemp}&#176;F</span>
-        </p> 
-        `;
-
-      createForecastTomorrow.innerHTML = `
-        <h4>Tomorrow</h4>
-        <p class="display-tomorrow">
-        <span><strong>Average Temperature: </strong>${tomorrowAvgTemp}&#176;F</span>
-        <span><strong>Max Temperature: </strong>${tomorrowMaxTemp}&#176;F</span>
-        <span><strong>Min Temperature: </strong>${tomorrowMinTemp}&#176;F</span>
-        </p> 
-        `;
-
-      createForecastNextTwoDays.innerHTML = `
-        <h4>Day After Tomorrow</h4>
-        <p class="display-next-two-days">
-        <span><strong>Average Temperature: </strong>${nextTwoDaysAvgTemp}&#176;F</span>
-        <span><strong>Max Temperature: </strong>${nextTwoDaysMaxTemp}&#176;F</span>
-        <span><strong>Min Temperature: </strong>${nextTwoDaysMinTemp}&#176;F</span>
-        </p> 
-        `;
-
-        const selectHistoryDefaultText = document.querySelector("#history-default-text")
-        selectHistoryDefaultText.setAttribute("style", "display:none")
-        let createHistoryList = document.createElement("li")
-        createHistoryList.textContent = `${cityHeading} - ${cityCurrentlyFeelsLifeInFarenheit}\u00B0F`;
-        ul.append(createHistoryList)
-    });
-}
+            `;
+            
+            let todayAvgTemp = data.weather[0].avgtempF;
+            let todayMaxTemp = data.weather[0].maxtempF;
+            let todayMinTemp = data.weather[0].mintempF;
+            let tomorrowAvgTemp = data.weather[1].avgtempF;
+            let tomorrowMaxTemp = data.weather[1].maxtempF;
+            let tomorrowMinTemp = data.weather[1].mintempF;
+            let nextTwoDaysAvgTemp = data.weather[2].avgtempF;
+            let nextTwoDaysMaxTemp = data.weather[2].maxtempF;
+            let nextTwoDaysMinTemp = data.weather[1].mintempF;
+            
+            createForecastToday.innerHTML = `
+            <h4>Today</h4>
+            <p class="display-today">
+            <span><strong>Average Temperature: </strong>${todayAvgTemp}&#176;F</span>
+            <span><strong>Max Temperature: </strong>${todayMaxTemp}&#176;F</span>
+            <span><strong>Min Temperature: </strong>${todayMinTemp}&#176;F</span>
+            </p> 
+            `;
+            
+            createForecastTomorrow.innerHTML = `
+            <h4>Tomorrow</h4>
+            <p class="display-tomorrow">
+            <span><strong>Average Temperature: </strong>${tomorrowAvgTemp}&#176;F</span>
+            <span><strong>Max Temperature: </strong>${tomorrowMaxTemp}&#176;F</span>
+            <span><strong>Min Temperature: </strong>${tomorrowMinTemp}&#176;F</span>
+            </p> 
+            `;
+            
+            createForecastNextTwoDays.innerHTML = `
+            <h4>Day After Tomorrow</h4>
+            <p class="display-next-two-days">
+            <span><strong>Average Temperature: </strong>${nextTwoDaysAvgTemp}&#176;F</span>
+            <span><strong>Max Temperature: </strong>${nextTwoDaysMaxTemp}&#176;F</span>
+            <span><strong>Min Temperature: </strong>${nextTwoDaysMinTemp}&#176;F</span>
+            </p> 
+            `;
+            
+            const selectHistoryDefaultText = document.querySelector(
+                "#history-default-text"
+                );
+                selectHistoryDefaultText.setAttribute("style", "display:none");
+                let createHistoryList = document.createElement("li");
+                createHistoryList.textContent = `${cityHeading} - ${cityCurrentlyFeelsLifeInFarenheit}\u00B0F`;
+                ul.append(createHistoryList);
+                
+                // let previousCity = 
+            });
+        }
+        
+        
